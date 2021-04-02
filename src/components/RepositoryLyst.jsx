1abf34bf9b68ff67fb1ba11e6c74db005781d425
Yan-Gurgel <yan.gurgel@gmail.com>
2021-04-02 14:40:31 -0300

    Utilizando useEffect

--- src/components/RepositoryLyst.jsx
+++ src/components/RepositoryLyst.jsx
@@ -1,22 +1,31 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
-
 import '../styles/repositories.scss';
+
+//https://api.github.com/orgs/rocketseat/repos
 
 const repository = {
     name: 'unform',
     description: 'Forms in React',
     link: 'https://www.google.com.br/'
 }
-
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []);
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                <RepositoryItem repository = {repository}/>
-                <RepositoryItem repository = {repository}/>
-                <RepositoryItem repository = {repository}/>
-                <RepositoryItem repository = {repository}/>
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
             </ul>
         </section>
     );
