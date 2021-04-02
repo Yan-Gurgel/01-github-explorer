daa1e4a4db09ec6993982dec4c63d6f043a1a4e6
Yan-Gurgel <yan.gurgel@gmail.com>
2021-04-02 15:07:55 -0300

    Listagem de Repositorios

--- src/components/RepositoryLyst.jsx
+++ src/components/RepositoryLyst.jsx
@@ -4,11 +4,6 @@ import '../styles/repositories.scss';
 
 //https://api.github.com/orgs/rocketseat/repos
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://www.google.com.br/'
-}
 export function RepositoryList() {
     const [repositories, setRepositories] = useState([]);
 
@@ -22,10 +17,9 @@ export function RepositoryList() {
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
             </ul>
         </section>
     );
