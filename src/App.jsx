eae6fcde8fbd6e00e4bf3359a93db7299e652a70
Yan-Gurgel <yan.gurgel@gmail.com>
2021-04-02 11:57:50 -0300

    Estado do Componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Counter } from './components/Counter';
 import { RepositoryList } from './components/RepositoryLyst';
 import './styles/global.scss';
 
 export function App() {
 
-  return <RepositoryList />
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  )
 }
